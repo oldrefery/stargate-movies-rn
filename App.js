75159830a763d71d9c418935a652fb1b2287e0d6
oldrefery <35261486+oldrefery@users.noreply.github.com>
2021-08-05 18:23:32 +0300

    feat: Add fetch data, component for movie card

--- App.js
+++ App.js
@@ -1,12 +1,38 @@
-import React from 'react';
-import {View} from 'react-native';
+import React, {useEffect, useState} from 'react';
+import {FlatList, Text, View} from 'react-native';
 
 import Header from './src/components/Header';
+import ImageCard from './src/components/ImageCard';
+
+const url =
+  'https://s3.eu-central-1.wasabisys.com/ghashtag/RNForKids/00-Init/data.json';
 
 const App = () => {
+  const [movies, setMovies] = useState([]);
+
+  useEffect(() => {
+    fetch(url)
+      .then(response => response.json())
+      .then(data => setMovies(data))
+      .catch(error => console.log(error.message));
+  }, []);
+
+  const keyExtractor = item => {
+    return `id-${item.id}`;
+  };
+
+  const renderItem = ({item}) => {
+    return <ImageCard movie={item} />;
+  };
+
   return (
     <View>
       <Header title={'STAR GATE'} />
+      <FlatList
+        data={movies}
+        renderItem={renderItem}
+        keyExtractor={keyExtractor}
+      />
     </View>
   );
 };
---
+++ src/components/ImageCard.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import {Image, StyleSheet, Text, View} from 'react-native';
+
+const ImageCard = ({movie}) => {
+  return (
+    <View style={styles.container}>
+      <Text>{movie.name}</Text>
+      <Text>{movie.info}</Text>
+      <Text>{movie.year}</Text>
+      <Image source={{uri: movie.image}} style={{width: 200, height: 200}} />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    borderWidth: 1,
+  },
+});
+
+export default ImageCard;
